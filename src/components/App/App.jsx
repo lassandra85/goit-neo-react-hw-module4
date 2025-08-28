64a5988ac0eac72974c425fbfb22c341775e4a09
lassandra85 <lassandra85@gmail.com>
2025-08-28 22:38:40 +0200

    new

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+//import { useState } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
+
+/* const App = () => {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleSearch = async topic => {
+        try {
+            setArticles([]);
+            setError(false);
+            setLoading(true);
+
+            const data = await fetchArticlesWithTopic(topic);
+            setArticles(data);
+        } catch (error) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+    return (
+        <div>
+            <SearchBar onSearch={handleSearch} />
+            {loading && <Loader />}
+            {error && <Error />}
+            {articles.length > 0 && <ArticleList items={articles} />}
+        </div>
+    );
+}; */
+
+//export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,30 @@
+//import css from './SearchBar.module.css';
+
+const SearchBar = ({ onSearch }) => {
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        const form = evt.target;
+        const topic = form.elements.topic.value;
+
+        // Якщо текстове поле порожнє, виводимо повідомлення
+        // і припиняємо виконання функції.
+        if (form.elements.topic.value.trim() === '') {
+            alert('Please enter search term!');
+            return;
+        }
+
+        // У протилежному випадку викликаємо пропс
+        // і передаємо йому значення поля
+        onSearch(topic);
+        form.reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="topic" placeholder="Пошук статей..." />
+            <button>Пошук</button>
+        </form>
+    );
+};
+
+export default SearchBar;
