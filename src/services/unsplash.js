c5fe8157de95a32cccbb0cfa2027d6c936f8817c
lassandra85 <lassandra85@gmail.com>
2025-08-31 13:43:42 +0200

    new

--- src/services/unsplash.js
+++ src/services/unsplash.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const ACCESS_KEY = 'GG_eLVvRzJH2AoVwMxJJSmpkjplCCyN7huwOCGdcbh8';
+
 const api = axios.create({
     baseURL: 'https://api.unsplash.com',
     params: {
-        client_id: import.meta.env.VITE_UNSPLASH_ACCESS_KEY,
+        client_id: ACCESS_KEY,
     },
 });
 
@@ -11,7 +13,7 @@ export async function searchPhotos({ query, page = 1, perPage = 12 }) {
     const { data } = await api.get('/search/photos', {
         params: { query, page, per_page: perPage, orientation: 'landscape' },
     });
-    return data; // { total, total_pages, results: [...] }
+    return data;
 }
 
 export default { searchPhotos };
